f14863acd54eac6d3fad05b4a7574b32cfe6fb3e
YellowCool <40266062+yellowcools@users.noreply.github.com>
2020-10-03 23:23:19 +0800

    Add SharpenFilter,FindEdgesFilter,BlurFilter,EmbossFilter,BGRFuncFilter,BGRCurveFilter,BGRPortraCurveFilter

--- cameo.py
+++ cameo.py
@@ -6,6 +6,7 @@
 # @Software: python3.7
 
 import cv2
+import filters
 from managers import WindowManager, CaptureManager
 
 class Cameo(object):
@@ -13,12 +14,18 @@ class Cameo(object):
     def __init__(self):
         self._windowManager = WindowManager('Cameo', self.onKeypress)
         self._captureManager = CaptureManager(cv2.VideoCapture(0,cv2.CAP_DSHOW), self._windowManager, True)
+        self._curveFilter = filters.BGRPortraCurveFilter()
 
     def run(self):
+        """Run the main loop."""
+
         self._windowManager.createWindow()
         while self._windowManager.isWindowCreated:
             self._captureManager.enterFrame()
             frame = self._captureManager.frame
+
+            filters.strokeEdges(frame, frame)
+            self._curveFilter.apply(frame, frame)
 
             self._captureManager.exitFrame()
             self._windowManager.processEvents()
